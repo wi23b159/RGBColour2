c02e4f9e1b4d7850848ef6a82d17094aed4baeb7
Kiyavash <wi23b159@technikum-wien.at>
2024-10-14 15:35:25 +0200

    adding complementary and rgbservice

---
+++ src/main/java/at/technikum/Complementary.java
@@ -0,0 +1,22 @@
+package at.technikum;
+
+public class ComplementaryService {
+    public String getComplementaryColor(String color) {
+        switch (color.toLowerCase()) {
+            case "red":
+                return "green";
+            case "green":
+                return "red";
+            case "blue":
+                return "orange";
+            case "orange":
+                return "blue";
+            case "yellow":
+                return "purple";
+            case "purple":
+                return "yellow";
+            default:
+                throw new IllegalArgumentException("Color not supported");
+        }
+    }
+}
